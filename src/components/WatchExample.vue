89291042723b93a29513ecf7464380a92aa6ec7a
= <=>
2024-11-21 17:39:01 +0100

    add files

---
+++ src/components/WatchExample.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { reactive, watch } from 'vue'
+const numbers = reactive([])
+
+watch(
+  () => numbers[numbers.length - 1],
+  (newValue, oldValue) =>
+    console.log(
+      'new number added: ',
+      numbers[numbers.length - 1],
+      'nouValor: ',
+      newValue,
+      'oldValue: ',
+      oldValue,
+    ),
+  { deep: true },
+)
+
+const randomNum = () => numbers.push((Math.random() * 100).toFixed(0))
+</script>
+<template>
+  <button @click="randomNum">Add Random Number</button>
+</template>
